94aa380eb6688fbb964aebd3454bb0d269ffdb84
Jetlove19 <rbrorlove@gmail.com>
2022-12-08 11:31:26 +0300

    Добавьте файлы проекта.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			string myName;
+			myName = "Любовь";
+
+			Console.WriteLine(myName);
+
+			Console.ReadKey();
+		}
+	}
+}
